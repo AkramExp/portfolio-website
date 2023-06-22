89dd1e12a3db7411566a1134aa259233a9d29490
Akram Ali <akram.exp02@gmail.com>
2023-06-22 21:22:00 +0530

    email sending form completed

--- pages/Contact Me.py
+++ pages/Contact Me.py
@@ -1,8 +1,17 @@
 import streamlit as st
+from send_email import sendMail
 
 st.title('Contact Me')
 
 with st.form(key='email_form'):
-    email = st.text_input("Enter Your Email")
-    message = st.text_area("Enter Your Message")
+    user_email = st.text_input("Enter Your Email")
+    raw_message = st.text_area("Enter Your Message")
+    message = f"""\
+Subject: An Email from {user_email}
+
+From: {user_email}
+{raw_message}
+"""
     button = st.form_submit_button("SUBMIT")
+    if button:
+        sendMail(message)
