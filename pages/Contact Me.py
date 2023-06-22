78833fe8239cd4488b2cbc741edc7d7a75038175
Akram Ali <akram.exp02@gmail.com>
2023-06-22 20:11:49 +0530

    add a form in contact me page with basic layout

--- pages/Contact Me.py
+++ pages/Contact Me.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title('Contact Me')
+
+with st.form(key='email_form'):
+    email = st.text_input("Enter Your Email")
+    message = st.text_area("Enter Your Message")
+    button = st.form_submit_button("SUBMIT")
